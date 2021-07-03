22ce90a0a4673684abf4d905e8934932eb8cb8f8
makaveli <prakasbudiawan@gmail.com>
2021-07-03 10:33:18 +0700

    Update for api and js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items: [],
+      isLoaded: true
+    };
+  }
+
+  componentDidMount() {
+    fetch("/demi")
+      .then(res => res.json())
+      .then(res => {
+        // this.setState({
+        //   isLoaded: true,
+        //   items: res.data
+        // });
+        console.log(res.data)
+      });
+  }
+
+  render() {
+    // return(
+
+      var { items, isLoaded } = this.state;
+      // var itemInfo = items.map(item => <div key={item.id}>Hex:{item.name}</div>);
+      // console.log(itemInfo)
+      return(
+              <table id="example" className="table table-striped table-bordered table-hover">
+              <thead>
+                  <tr>
+                      <th>Username</th>
+                  </tr>
+              </thead>
+              <tbody>
+              {
+                    items.map((item) => (
+                        <tr key={item.id}>
+                            <td>{item.id}</td>
+                        </tr>
+                        // console.log(item)
+                    ))
+                }
+                
+              </tbody>
+          </table>
+      )
+      
+    // ...
+  }
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import Crud from './components/Crud'
+// import Main from './Main'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    {/* <Main /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
