c7c85ed6e413280354e9b85ef6ce5f8e383b1499
makaveli <prakasbudiawan@gmail.com>
2021-07-03 11:42:36 +0700

    Fixed

--- api/api.py
+++ api/api.py
@@ -41,89 +41,6 @@ class Matkul(db.Model):
       def __repr__(self):
             return '<Matkul %r>' % self.id_matkul
 
-@app.route('/join', methods=['GET'])
-def join():
-      data = db.session.query(Matkul.id_matkul, Matkul.nama_matkul, Mahasiswa.name, Mahasiswa.email, Mahasiswa.id_matkul) \
-            .filter(Matkul.id_matkul==Mahasiswa.id_matkul) \
-            .filter(Matkul.id_matkul==1) \
-            .all()
-      print(f'{data}')
-      x = transform(data)
-      return make_response(jsonify({'data': x}), 200)
-
-@app.route('/where', methods=['POST'])
-def defrtu():
-      request_json         = request.get_json()
-      name             = request_json.get('name')       
-      email             = request_json.get('email')
-
-      data = db.session.query(Mahasiswa) \
-            .filter(func.lower(Mahasiswa.name) == func.lower(name)) \
-            .all()
-      
-      datax = db.session.query(Mahasiswa) \
-            .filter(func.lower(Mahasiswa.email) == func.lower(email)) \
-            .all()
-
-      if data and datax:
-            return make_response(jsonify({'msg':'Username and Email has been used', 'status_code': 409}), 200)
-
-      if data:
-            return make_response(jsonify({'msg':' users has been used', 'status_code': 409}), 200)
-
-      if datax:
-            return make_response(jsonify({'msg':' email has been used', 'status_code': 409}), 200)
-      else:
-
-            print(f'{data}')
-            x = transform(data)
-      return make_response(jsonify({'msg' : 'Yayy'}), 200)
-
-@app.route('/getData', methods=['GET'])
-def getById():
-      data = db.session.query(Matkul.id_matkul, Matkul.nama_matkul, Mahasiswa.name, Mahasiswa.email, Mahasiswa.id_matkul) \
-            .filter(Matkul.id_matkul==Mahasiswa.id_matkul) \
-            .filter(Mahasiswa.id == '33') \
-            .all()
-      print(f'{data}')
-      x = transform(data)
-      return make_response(jsonify({'data': x}), 200)
-
-@app.route('/time', methods=['GET'])
-def timeid():
-      data = format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-      return make_response(jsonify({'msg': data}), 200)
-
-@app.route('/getMatkul', methods=['GET'])
-def getMatkulO():
-      data = db.session.query(Matkul).count()
-      print(f'{data}')
-      # x = transform_mt(data)
-      return make_response(jsonify({'data': data}), 200)
-
-
-
-@app.route('/api')
-def index():
-#      try:
-           myMahasiswa = Mahasiswa.query.all()
-           print('\n### All Users:')
-           for mhs in myMahasiswa:
-                 print(f'{mhs.name} with ID {mhs.id}')
-                 data = transform(myMahasiswa)
-                 return make_response(jsonify({'data': data}), 200)
-            #      except Exception as e:
-            #            print(e)
-
-@app.route('/profile/<name>')
-def dre(name):
-      myMahasiswa = Mahasiswa.query.filter_by(name=name).first()
-      print('\n### All Users:')
-      for mhs in myMahasiswa:
-            print(f'{mhs.name} with ID {mhs.id}')
-            data = transform(myMahasiswa)
-            return make_response(jsonify({'data': data}), 200)
-
 @app.route('/post', methods=['POST'])
 def post_u():
       request_json     = request.get_json()
@@ -135,9 +52,7 @@ def post_u():
             2)
       db.session.add(mahasiswa)
       db.session.flush()  
-      # print(mahasiswa.id)
-      # mahasiswa.i
-      # db.session.refresh(mahasiswa)
+     
       db.session.commit()
       idx = mahasiswa.id
       vins = [idx]
@@ -145,12 +60,9 @@ def post_u():
       x = transform_mt(datap)
 
       for idk in vins:
-            # print(idk)
-            # n = list(idk)
+            
             for data in x:
                   
-            #       s = ''.join(n)
-                  # print(idk, data['matkul_id'])
                   sam = ''.join(str(e) for e in vins)
                   # print(sam)
                   idf = data['matkul_id']
@@ -158,45 +70,12 @@ def post_u():
                         idk,
                         data['matkul_id']
                         )
-                  # db.session.add(nk)
-                  # db.session.flush()  
-                  # db.session.commit()
-      # print(mahasiswa.id)
-      # mahasiswa.i
-      # db.session.refresh(mahasiswa)
-      # db.session.commit()
-                  # junkirk = data['matkul_id']
-            # print(f'{data}')
-            # fres = {
-            #       'id_mah'   : idf,
-            #       'id_mod' : id_mod
-            # }
+     
       return jsonify({'data': sam}), 200
 
 @app.route('/demi', methods=['GET'])  
 def loli():
       
-      # search   = request.args.get('search')
-      # # sql = text('select * from Matkul where nama_matkul =:matkul',{'matkul': 'ipa'})
-      # # indo = 'ipa'
-      # # result = db.engine.execute(
-      # #       text('select id_matkul,nama_matkul from Matkul where nama_matkul = :matkul'),
-      # #       matkul = indo
-      # # )
-
-      # # result = db.session.query(Mahasiswa).filter(~Matkul.id_matkul.in_(Mahasiswa.id_matkul)).all()
-      # # subq = db.session.query(Mahasiswa.id_matkul)
-      
-      # # x = transform_mt(subq)
-
-      # result = db.session.query(Matkul.id_matkul).filter(Matkul.id_matkul != 4).filter(Matkul.id_matkul.notin_(db.session.query(Mahasiswa.id_matkul))).all()
-      # # result = db.session.query(Mahasiswa.id).all()
-      # # data = transform_mt(result)
-
-      # # resultx = db.session.query(Matkul)
-
-      # # if search:
-
       result = db.session.query(Matkul).order_by(Matkul.id_matkul.desc()).all()
 
       data = transform(result)
@@ -208,46 +87,23 @@ def loli():
             findX = db.session.query(Mahasiswa).filter(Mahasiswa.id_matkul == idops).count()
             x.append(findX)
             print(findX)
-      # j = data[0]['nama_matkul']
-      # print(j)
+     
      
       return jsonify({'data' : data, 'cums':x})
 
-@app.route('/nubi', methods=['GET'])  
-def nubi():
-      s = 'hubs', 'maks'
 
-      # print(loki)
-      x = []
-      for i in s:
-            # x = i
-            x.append(i)
-            print(x)
-      # loki = ",".join(s) 
-      return make_response(jsonify({'data' : x}))
-
-# def fuls(data):
-
-#       for 
 def singleTransform(x):
-      # data = {
-      #       'id': myMahasiswa.id,
-      #       'name': myMahasiswa.name,
-      #       }
+
       data = {
-            # 'nama_matkul' : x.nama_matkul
             
-                  'matkul_id' : x.id_matkul,
-                  'nama_matkul' : x.nama_matkul
+            'matkul_id' : x.id_matkul,
+            'nama_matkul' : x.nama_matkul
                   
-            }
+      }
       return data
 
 def singleMatkul(xd):
-      # data = {
-            # 'id': xd.id
-            # 'name': myMahasiswa.name,
-            # }
+     
       data = {
             'matkul_id' : xd.id_matkul,
       #       # 'nama_matkul' : xd.nama_matkul
--- src/App.js
+++ src/App.js
@@ -14,42 +14,19 @@ class App extends Component {
     fetch("/demi")
       .then(res => res.json())
       .then(res => {
-        // this.setState({
-        //   isLoaded: true,
-        //   items: res.data
-        // });
         console.log(res.data)
       });
   }
 
   render() {
-    // return(
 
       var { items, isLoaded } = this.state;
-      // var itemInfo = items.map(item => <div key={item.id}>Hex:{item.name}</div>);
-      // console.log(itemInfo)
       return(
-              <table id="example" className="table table-striped table-bordered table-hover">
-              <thead>
-                  <tr>
-                      <th>Username</th>
-                  </tr>
-              </thead>
-              <tbody>
-              {
-                    items.map((item) => (
-                        <tr key={item.id}>
-                            <td>{item.id}</td>
-                        </tr>
-                        // console.log(item)
-                    ))
-                }
-                
-              </tbody>
-          </table>
+          <div>
+            Hello Makaveli
+          </div>
       )
       
-    // ...
   }
 }
 
