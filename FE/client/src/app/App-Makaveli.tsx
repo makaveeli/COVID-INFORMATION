fd613d1462d21091160000ba487b74be97d25051
makaveli <prakasbudiawan@gmail.com>
2021-07-18 16:58:55 +0700

    Change legend

--- FE/client/src/app/App-Makaveli.tsx
+++ FE/client/src/app/App-Makaveli.tsx
@@ -113,7 +113,7 @@ class AppMakaveli extends React.Component<{}, myProps> {
       labels: result.data.provinsi,
       datasets: [
         {
-          label: 'Positif Case',
+          label: 'Death Case',
           backgroundColor: [
             "#69d2e7","#a7dbd8","#e0e4cc","#f38630","#fa6900",
             "#fe4365","#fc9d9a","#f9cdad","#c8c8a9","#83af9b",
@@ -254,7 +254,7 @@ class AppMakaveli extends React.Component<{}, myProps> {
       labels: result.data.provinsi,
       datasets: [
         {
-          label: 'Positif Case',
+          label: 'Recover Case',
           backgroundColor: [
             "#69d2e7","#a7dbd8","#e0e4cc","#f38630","#fa6900",
             "#fe4365","#fc9d9a","#f9cdad","#c8c8a9","#83af9b",
